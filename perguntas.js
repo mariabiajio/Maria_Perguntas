0ffb65d1f80a2a9bef0da2207182b2f986fa9b50
mariabiajio <168825218+mariabiajio@users.noreply.github.com>
2024-11-28 13:16:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'beach tennis',
+    'Qual é a altura oficial da rede no beach tennis?',
+    '1,70 metros.'
+)
+
+criaCartao(
+    'beach tennis',
+    'Qual é o sistema de pontuação no beach tennis?',
+    'Igual ao tênis tradicional: 15, 30, 40, game.'
+)
+
+criaCartao(
+    'beach tennis',
+    'Quais acessórios são essenciais para jogar beach tennis?',
+    'Raquete, bola, óculos de proteção e roupa confortável.'
+)
+
+criaCartao(
+    'beach tennis',
+    'O beach tennis é jogado com qual tipo de bola?',
+    'Bola de tênis despressurizada.'
+)
